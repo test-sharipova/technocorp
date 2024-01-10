5762bff603bb7d819fe425ce71c2570438b6e7db
DinaSharipova <sharipova0308@gmail.com>
2024-01-10 12:19:07 +0500

    add mobile edits

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 //меню
 const menu = document.querySelector('.menu'),
-menuItem = document.querySelectorAll('.menu__item'),
+menuItem = document.querySelectorAll('.item'),
 hamburger = document.querySelector('.hamburger');
 
 hamburger.addEventListener('click', () => {
@@ -14,6 +14,50 @@ menuItem.forEach(item => {
 		menu.classList.toggle('menu_active');
 	});
 });
+//скрываются пункты меню при сужении экрана
+function responseMenu(){
+	if (window.innerWidth < 768) return; 
+	$('ul.dropdown-menu li.item').appendTo('ul.menu');
+	var items = $('ul.menu li.item');
+	var max_width = $('ul.menu').width() - $('ul.menu li.dd_menu').outerWidth();
+	var width = 0;
+	var hide_from = 0;
+
+	items.css({'width':'auto'});
+
+	items.each(function(index){
+		if (width + $(this).outerWidth() > max_width)
+		{
+			return false;
+		}
+		else
+		{
+			hide_from = index;
+			width += $(this).outerWidth();
+		}
+	});
+	if (hide_from < items.length - 1) {
+		items.eq(hide_from).nextAll('li.item').appendTo('ul.dropdown-menu');
+		// items.css({'width':(max_width / (hide_from)) + 'px'});
+        items.css({'width':'fit-content'});
+		$('ul.menu li.dd_menu').show();
+	}
+	else
+	{
+		$('ul.menu li.dd_menu').hide();
+	}
+}
+
+
+
+	$('.top_menu').on('click', '.dropdown-toggle', function () {
+		$('.dropdown-menu').toggle();
+	});
+
+	$(window).on('resize', function(){
+		responseMenu();
+	}).trigger('resize');
+
 
 //телефон в моб версии
 $('.header__contacts__icon').on('click', function(){
